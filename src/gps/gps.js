b13f16631c91b6ea6855a50c6efe75526393e5fb
PiIsFour <PiIsFour@users.noreply.github.com>
2018-04-22 19:24:42 +0200

    gpsStream pushes errrors to the stream (#4)
    also set enableHighAccuracy to true for testing

--- src/gps/gps.js
+++ src/gps/gps.js
@@ -1,5 +1,13 @@
 import stream from '../stream/stream'
 
 export function gpsStream (geolocation) {
-	return stream(s => geolocation.watchPosition(pos => s.push(pos)))
+	return stream(s => {
+		geolocation.watchPosition(pos => s.push(pos),
+			error => s.pushError(error),
+			{
+				enableHighAccuracy: true,
+				// timeout: 5000,
+				maximumAge: 0
+			})
+	})
 }
