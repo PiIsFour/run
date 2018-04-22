7d0a8e6b85646054e7f67e7dec461b090b6a76c6
PiIsFour <PiIsFour@users.noreply.github.com>
2018-04-22 18:25:46 +0200

    stream handles errors with a catch

--- src/stream/stream.js
+++ src/stream/stream.js
@@ -1,10 +1,30 @@
 class Stream {
 	constructor () {
 		this.push = () => {}
+		this.pushError = () => {}
 	}
 	map (cb) {
 		const s = new Stream()
-		this.push = value => s.push(cb(value))
+		this.push = value => {
+			try {
+				return s.push(cb(value))
+			} catch (error) {
+				s.pushError(error)
+			}
+		}
+		this.pushError = error => s.pushError(error)
+		return s
+	}
+	catch (cb) {
+		const s = new Stream()
+		this.push = value => s.push(value)
+		this.pushError = error => {
+			try {
+				cb(error)
+			} catch (error) {
+				s.pushError(error)
+			}
+		}
 		return s
 	}
 }
--- src/stream/stream.test.js
+++ src/stream/stream.test.js
@@ -12,4 +12,63 @@ describe('stream', () => {
 		event(43)
 		expect(mock).toBeCalledWith(42)
 	})
+
+	it('pushed errors do not show up in map', () => {
+		const mock = jest.fn()
+		let event = () => {}
+		stream((stream) => {
+			event = value => stream.pushError(value)
+		})
+			.map(v => v - 1)
+			.map(mock)
+		const error = 'ERROR: Sorry something wrong happend'
+		event(error)
+		expect(mock).not.toBeCalled()
+	})
+
+	it('pushed errors show up in catch', () => {
+		const mock = jest.fn()
+		let event = () => {}
+		stream((stream) => {
+			event = value => stream.pushError(value)
+		})
+			.map(v => v - 1)
+			.catch(mock)
+		const error = 'ERROR: Sorry something wrong happend'
+		event(error)
+		expect(mock).toBeCalledWith(error)
+	})
+
+	it('redirects errors thrown inside map', () => {
+		const mock = jest.fn()
+		let event = () => {}
+		const error = new Error('ERROR: Sorry something wrong happend')
+		stream((stream) => {
+			event = value => stream.push(value)
+		})
+			.map(v => {
+				throw error
+			})
+			.catch(mock)
+		event(42)
+		expect(mock).toBeCalledWith(error)
+	})
+
+	it('allows errors to be rethrown inside catch', () => {
+		const mock = jest.fn()
+		let event = () => {}
+		const error = new Error('ERROR: Sorry something wrong happend')
+		stream((stream) => {
+			event = value => stream.push(value)
+		})
+			.map(v => {
+				throw error
+			})
+			.catch(e => {
+				throw e
+			})
+			.catch(mock)
+		event(42)
+		expect(mock).toBeCalledWith(error)
+	})
 })
